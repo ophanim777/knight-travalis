0bca691129748b1f6f25e7396ee0e6cbe8629336
ophanim777 <rikyjo49@gmail.com>
2026-03-08 22:53:27 +0700

    add  function to check if the movement is valid

--- javascript.js
+++ javascript.js
@@ -1,6 +1,11 @@
 function knightMoves(start, end) {
+
     const moves = [
   [2,1],[1,2],[-1,2],[-2,1],
   [-2,-1],[-1,-2],[1,-2],[2,-1]
 ];
+
+function isValidMove(x, y) {
+  return x >= 0 && x < 8 && y >= 0 && y < 8;
+}
 }
